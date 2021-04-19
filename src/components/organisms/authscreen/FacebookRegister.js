90edda79dbfe257470fe7051ce1ec5b9a269b268
Bipul Poudel <bipulpoudel55555@gmail.com>
2021-04-20 00:59:30 +0545

    [completed] facebook popup completed

--- src/components/organisms/authscreen/FacebookRegister.js
+++ src/components/organisms/authscreen/FacebookRegister.js
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Pressable, ToastAndroid } from "react-native";
+import { View, StyleSheet, Pressable, Text } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
-
-//Theme & Icons
 import { COLORS } from "../../../constants/theme";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-
-//Facebook Login Imports
 import { LoginManager, AccessToken } from "react-native-fbsdk";
 import { getDataFromFacebook } from "../../../services/authProvider/Facebook";
-
-//Redux Imports
-//Redux Imports
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { checkUser } from "../../../redux/actions/auth";
 import Toast from "react-native-simple-toast";
+import { Alert } from "react-native";
+import Dialog from "react-native-dialog";
+import { TextInput } from "react-native";
 
 const FacebookRegister = ({
   checkUser,
@@ -23,6 +19,9 @@ const FacebookRegister = ({
   userData: { receivedData, facebookLoginLoading },
 }) => {
   const [used, setUsed] = useState(false);
+  const [email, setEmail] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     if (receivedData && !facebookLoginLoading && used) {
@@ -48,22 +47,44 @@ const FacebookRegister = ({
       let data = await AccessToken.getCurrentAccessToken();
       let token = data?.accessToken;
       const userData = await getDataFromFacebook(token);
-      await checkUser(userData, "fb");
+
+      if (!userData.email) {
+        await checkUser(userData, "fb");
+      } else {
+        Alert.alert(
+          "Oops, Email is not available",
+          "Please enter your email address to continue",
+          [{ text: "Add New", onPress: () => setVisible(true) }]
+        );
+      }
     } else {
       alert("Login In Not Complete");
     }
   };
 
+  const onConfirmPress = async () => {
+    setVisible(false);
+    await checkUser({ email, name }, "fb");
+  };
+
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.buttonStyle} onPress={() => _fbAuth()}>
-        {facebookLoginLoading ? (
-          <ActivityIndicator color={COLORS.primary} size="small" />
-        ) : (
-          <FontAwesome name="facebook" color="#44619c" size={20} />
-        )}
-      </Pressable>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Pressable style={styles.buttonStyle} onPress={() => _fbAuth()}>
+          {facebookLoginLoading ? (
+            <ActivityIndicator color={COLORS.primary} size="small" />
+          ) : (
+            <FontAwesome name="facebook" color="#44619c" size={20} />
+          )}
+        </Pressable>
+      </View>
+      <Dialog.Container visible={visible}>
+        <Dialog.Title>Add Email Address</Dialog.Title>
+        <Dialog.Input onChangeText={setEmail} value={email} label="Email" />
+        <Dialog.Input onChangeText={setName} value={name} label="Name" />
+        <Dialog.Button label="Confirm" onPress={onConfirmPress} />
+      </Dialog.Container>
+    </>
   );
 };
 
